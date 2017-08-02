6ee5b3596f7b09dc9153da04f6524d4785119b9b
ballonj1 <ballonj1@tcnj.edu>
2017-08-01 22:10:10 -0700

    Add function to handle the visual representation of the passage of time

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -26,13 +26,15 @@ function handleRangeUpdate() {
 }
 
 function handleProgress() {
-  
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`
 }
 
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
